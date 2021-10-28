b7b06113a168e78ec3f80d905dcd1636693fe537
Johny <johnyj2@nih.gov>
2021-10-28 10:49:18 -0400

    Fix: added service for nginx

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -28,8 +28,8 @@ helm -n ${namespace} delete ${projectName}
 
 sed -i.bak \
     -e "s/DJANGO_ALLOWED_HOSTS_VALUE/${DJANGO_ALLOWED_HOSTS}/g" \
-    deployment.yaml
-rm deployment.yaml.bak
+    templates/deployment.yaml
+rm templates/deployment.yaml.bak
 
 echo $DJANGO_ALLOWED_HOSTS
 
